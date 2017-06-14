45f2c8789e920e9002c377d4dcbca7116b1fc002
Jonathan Kreugar <Jonathan Kreugar@AA597948-L1>
2017-06-14 10:42:47 -0400

    Add project files.

---
+++ GreyHoundRaceTrackSim/Bet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreyHoundRaceTrackSim
+{
+   public class Bet
+    {
+        public int Amount; // The amount of Cash that was bet 
+        public int Dog; // The number of dog the bet is on 
+        public Guy Bettor; // The guy who placed the bet 
+
+        public string GetDescription()
+        {
+            // Return a string that says who placed the bet, how much 
+            // cash was bet, and which dog he bet on ("Joe bet 8 on
+            // dog #4"). If the amount is zero, no bet was placed
+            //("Joe hasn't place a bet.").
+
+        }
+        public int Payout (int Winner)
+        {
+            //The parameter is the winner of the race. If the dog won,
+            // return the amount bet. Otherwise, return the regative of 
+            // the amount bet. 
+        }
+    }
+}
---
+++ GreyHoundRaceTrackSim/Guy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; 
+
+namespace GreyHoundRaceTrackSim
+{
+   public class Guy
+    {
+        public string Name; // The guy's name
+        public Bet MyBet; // An instance of Bet that has his bet
+        public int Cash; // How much cash he has 
+
+        // The last two field are the guy's GUI controls on the form 
+        public RadioButton MyRadioButton; // MyRadioButton 
+        public Label MyLabel; // My Label
+        
+        public void UpdateLabels()
+        {
+            // Set my label to my bet's description, and the label on my RadioButton 
+            // to show my cash ("Joe has 43 bucks")
+
+        } 
+        public void ClearBet() { } //Reset my bet so it's zero
+    }
+        public bool PlaceBet(int BetAmount, int DogToWin)
+    {
+        // Place a new bet annd store it in my bet field 
+        //Return true if the had enough money to bet 
+    }
+        public void Collect(int Winner)
+    {
+        //Ask my bet to pay out , clear my bet, and update my labels
+    }
+}
